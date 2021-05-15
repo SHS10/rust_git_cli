9e2623da4fad015e7536432260ac70577b7fa8ac
namelesslogger <johnshanie@gmail.com>
2021-05-15 22:25:24 +0100

    [add] cmd line args parsing to enum types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+use std::io::{self, Write};
+use std::str::FromStr;
+use std::env;
+
+// Basically stolen from the rust book and sample CLI repo, see here: https://github.com/rust-cli/cli-template/
+// Ideally, we would use this struct to hold all arguments passed from the user, 
+// The crate StructOpt will handle creating the struct from the cmd line args, handles error messages too, 
+// We can work off this for quick results
+// edit: I will not use StructOpt as I wanna do something else invloving some enum tomfoolery
+#[derive(Debug)]
+struct Cli {
+    cmd: Command
+}
+
+impl Cli {
+    fn from_args(args: &[String]) -> Cli {
+        let query = args[1].to_lowercase();
+        let cmd = Command::from_str(&query).unwrap();
+
+        Cli { cmd }
+    }
+}
+
+// I think having an enum to handle command control flow is handy and fairly useful, 
+// Buut, i am not very familiar with how they are used and haven't done it well yet
+// At least for now, with them we have a clear path to completeion
+#[derive(Debug, PartialEq)]
+enum Command {
+    Push,
+    Pull,
+    Fetch,
+    Status,
+    Diff,
+}
+
+impl FromStr for Command {
+    type Err = ();
+    fn from_str(input: &str) -> Result<Command, Self::Err> {
+        match input {
+            "push"  => Ok(Command::Push),
+            "pull"  => Ok(Command::Pull),
+            "fetch" => Ok(Command::Fetch),
+            "status"=> Ok(Command::Status),
+            "diff"  => Ok(Command::Diff),
+            _       => Err(()),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let cli_args = Cli::from_args(&args);
 }
